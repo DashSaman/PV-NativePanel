a87d412a1864878c77ecdd40504ad9155c7fe6cc
Saman Kalantari <140158372+DashSaman@users.noreply.github.com>
2026-08-31 18:26:59 +0330

    fix(auth): restore schema17 rotate function on rollback

--- db/migrations/0018_auth_refresh_reuse_context.down.sql
+++ db/migrations/0018_auth_refresh_reuse_context.down.sql
@@ -7,4 +7,9 @@ SET LOCAL statement_timeout = '120s';
 SET LOCAL ROLE pvnaive_owner;
 
 DROP FUNCTION IF EXISTS pvnaive.auth_refresh_session_metadata(bytea);
+DROP FUNCTION IF EXISTS pvnaive.auth_rotate_session(bytea,bytea,bytea,bytea,timestamptz);
+ALTER FUNCTION pvnaive.auth_rotate_session_v17(bytea,bytea,bytea,bytea,timestamptz)
+    RENAME TO auth_rotate_session;
+REVOKE ALL ON FUNCTION pvnaive.auth_rotate_session(bytea,bytea,bytea,bytea,timestamptz) FROM PUBLIC;
+GRANT EXECUTE ON FUNCTION pvnaive.auth_rotate_session(bytea,bytea,bytea,bytea,timestamptz) TO pvnaive_app;
 DELETE FROM pvnaive.schema_migrations WHERE version = 18;
